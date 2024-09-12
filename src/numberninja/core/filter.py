db66e593e729b6944dc5861ef5f2a3d5a1558896
Rene Moll <github@r-moll.nl>
2024-09-12 22:27:49 +0200

    feat: add basic amount and description filters for Transactions

--- src/numberninja/core/filter.py
+++ src/numberninja/core/filter.py
@@ -1,23 +1,43 @@
-
-
+import decimal
+import operator
+import re
 
 def filter_transactions(transactions, criteria):
-	"""
-	"""
+    """
+    TODO: when comparing amounts, use the currency 
+    """
 
-	result = transactions
+    result = transactions
 
-	if "date" in criteria:
-		if "from" in criteria["date"]:
-			result = [t for t in result if _matcher_from_date(t, criteria["date"]["from"])]
-		if "until" in criteria["date"]:
-			result = [t for t in result if _matcher_until_date(t, criteria["date"]["until"])]
+    if "date" in criteria:
+        if "from" in criteria["date"]:
+            result = [t for t in result if _matcher_from_date(t, criteria["date"]["from"])]
+        if "until" in criteria["date"]:
+            result = [t for t in result if _matcher_until_date(t, criteria["date"]["until"])]
 
-	return result
+    if "amount" in criteria:
+        ops = {
+            "<": operator.lt,
+            "<=": operator.le,
+            "==": operator.eq,
+            "!=": operator.ne,
+            ">=": operator.ge,
+            ">": operator.gt
+        }
+        op = ops[criteria["amount"]["operator"]]
+        amount = criteria["amount"]["value"]
+
+        result = [t for t in result if op(t["amount"], amount)]
+
+    if "description" in criteria:
+        needle = criteria["description"]["contains"]
+        result = [t for t in result if re.search(r"\b{}\b".format(needle), t["description"], re.IGNORECASE)]
+
+    return result
 
 def _matcher_from_date(t, date):
-	return t["date"] >= date
+    return t["date"] >= date
 
 def _matcher_until_date(t, date):
-	return t["date"] <= date
+    return t["date"] <= date
 
